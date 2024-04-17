f0f8e44752de0ed9ca915c1b4ea9b4af6e5683db
Diggo <rdiggosilva@gmail.com>
2024-04-17 20:32:20 -0300

    Initial commit for Pokemon App

---
+++ Pokemon/Bindable.swift
@@ -0,0 +1,29 @@
+//
+//  Bindable.swift
+//  Pokemon
+//
+//  Created by Diggo Silva on 17/04/24.
+//
+
+import Foundation
+
+class Bindable<T> {
+    
+    init(value: T) {
+        self.value = value
+    }
+    
+    var value: T {
+        didSet {
+            observers.forEach { observe in
+                observe(value)
+            }
+        }
+    }
+    
+    fileprivate var observers: [((T) -> ())] = []
+    
+    func bind(observer: @escaping(T) ->()) {
+        self.observers.append(observer)
+    }
+}
